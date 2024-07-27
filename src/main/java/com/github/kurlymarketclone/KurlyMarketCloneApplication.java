5e5b5639838bf99cf0432312b0f400904b07a986
limhhyeon <honghyeon1007@gmail.com>
2024-07-27 15:09:20 +0900

    Initial commit

---
+++ src/main/java/com/github/kurlymarketclone/KurlyMarketCloneApplication.java
@@ -0,0 +1,13 @@
+package com.github.kurlymarketclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurlyMarketCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KurlyMarketCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/kurlymarketclone/KurlyMarketCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kurlymarketclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurlyMarketCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
